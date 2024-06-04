178ed8481164ebb9221e6b938f5cd38fa5a7878f
rutile <johan_lefol@yahoo.com>
2024-06-04 17:55:27 +0200

    full look finished

--- projets.js
+++
@@ -1,6 +0,0 @@
-const prjt1 = document.querySelector('#projet1');
-const openBtn = document.querySelector('#btnTest');
-
-openBtn.addEventListener('click', function (e) {
-    prjt1.classList.add('show');
-});
